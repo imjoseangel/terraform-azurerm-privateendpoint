3c6a6c133fcc64c99aa71771d1e522acdf98925c
imjoseangel <josea.munoz@gmail.com>
2022-01-30 23:04:10 +0100

    add count

--- outputs.tf
+++ outputs.tf
@@ -15,30 +15,30 @@ output "resource_group_location" {
 
 output "private_endpoint_id" {
   description = "The id of the private endpoint"
-  value       = azurerm_private_endpoint.main.id
+  value       = azurerm_private_endpoint.main.*.id
 }
 
 output "private_endpoint_nic" {
   description = "The nic connected to the private endpoint"
-  value       = azurerm_private_endpoint.main.network_interface
+  value       = azurerm_private_endpoint.main.*.network_interface
 }
 
 output "private_dns_zone_group" {
   description = "The ID of the Private DNS Zone Group."
-  value       = azurerm_private_endpoint.main.private_dns_zone_group.*.id
+  value       = azurerm_private_endpoint.main.*.private_dns_zone_group.id
 }
 
 output "custom_dns_configs" {
   description = "The custom DNS configurations."
-  value       = azurerm_private_endpoint.main.custom_dns_configs
+  value       = azurerm_private_endpoint.main.*.custom_dns_configs
 }
 
 output "private_dns_zone_configs" {
   description = "The Private DNS Zone Configs"
-  value       = azurerm_private_endpoint.main.private_dns_zone_configs
+  value       = azurerm_private_endpoint.main.*.private_dns_zone_configs
 }
 
 output "private_service_connection_ip" {
   description = "(Computed) The private IP address associated with the private endpoint, note that you will have a private IP address assigned to the private endpoint even if the connection request was Rejected"
-  value       = azurerm_private_endpoint.main.private_service_connection.*.private_ip_address
+  value       = azurerm_private_endpoint.main.*.private_service_connection.private_ip_address
 }
